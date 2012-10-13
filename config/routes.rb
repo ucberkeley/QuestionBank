872ef625f33acfe0acae1b6309f0eb76ef329d6c
Joseph McKenney <j.mckenney00@gmail.com>
2012-10-13 11:57:24 -0700

    added home page, routing changes and login forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 QuestionBank::Application.routes.draw do
+  get "home/index"
+
   resources :questions
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ QuestionBank::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
