0ba9d747cce3bb485dabbb13fe9b1d96a676da51
Alexander Chen <alex7c@gmail.com>
2012-10-28 03:01:10 -0700

    Hardcode Twitter Oauth tokens

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  # provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :twitter, 'qcHsSNwiktUsMXqV58CwA', 'KiDTjPMiXZQZU14tX3HQMl8eFXzoRlLOkroBE1ouOU'
 end
