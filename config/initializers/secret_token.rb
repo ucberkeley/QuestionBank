f0287dfb118327db1c7c2515804926e3ea0a5c95
Alexander Chen <alex7c@gmail.com>
2012-10-28 02:41:23 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionBank::Application.config.secret_token = '2894b9576b3d688c7e5fb30e612555ba3d22fab28c886a158cc71397bda29944a5cb7bf2b3330446d7c71952922317569003968c599957d4dd839c9a6c586316'
