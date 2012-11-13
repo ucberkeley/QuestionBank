d2421ee68871e845dae965e847e2e0aceeb96fda
Alexander Chen <alex7c@gmail.com>
2012-11-12 23:38:31 -0800

    Example: filter by ability

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.accessible_by(current_ability)
 
     respond_with @questions
   end
