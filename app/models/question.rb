b471de783279f5935942ecb370f65c3f356e908c
chrissy <christinedonov@gmail.com>
2012-11-17 23:46:54 -0800

    Fixed a bad change.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ActiveRecord::Base
-	has_and_belongs_to_many :tag
-  has_and_belongs_to_many :question_group
+	has_and_belongs_to_many :tags
+  has_and_belongs_to_many :question_groups
 	belongs_to :user
-	has_many :attempt
+	has_many :attempts
 
 	 def self.get_quiz(number_of_questions, tag_name)
     # Wonder if there is a cleaner way to do this
