32605d349c3d59a351f7e6207ffc82772c0b5b9a
chrissy <christinedonov@gmail.com>
2012-11-17 20:24:57 -0800

    Sad path of get_attempts works.

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,18 +1,26 @@
 class QueriesController < ApplicationController
   def get_attempts
-    if params[:user_group].blank?
-    
+    if !params[:user_group].blank?
+      user_group = params[:user_group]
+      if !params[:question_group].blank?
+        question_group = params[:question_group]
+        @attempts = Attempt.where(:user_group => user_group, :question_group => question_group).includes(:all)
+      else
+        @attempts = Attempt.where(:user_group => user_group).includes(:all)
+      end
+    else
+      if !params[:question_group].blank?
+        @attempts = Attempt.where(:question_group => question_group).includes(:all)
+      else
+        flash[:error] = 'You must choose either a user group or a question group.'
+        redirect_to :action => 'prepare_to_get_attempts' 
+      end
     end
-    if params[:question_group].blank?
-
-    end
-    user_group = params[:user_group]
-    question_group = params[:question_group]
   end
 
   def prepare_to_get_attempts
     # this is just for now
-    @user_groups = UserGroup.all
+    @user_groups = UserGroup.with_role(:viewer, current_user)
     # this can stay
     @question_groups = QuestionGroup.where(:user_id => current_user)
   end
