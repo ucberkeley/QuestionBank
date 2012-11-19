5522f0042bac203769526845d12d9f3a45dc1e8f
Alexander Chen <alex7c@gmail.com>
2012-11-18 23:00:42 -0800

    Prevent initialization on precompile

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,7 @@ module QuestionBank
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
