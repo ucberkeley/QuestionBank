fe1bfb7640bd6b3e79029eb0a60d18d678f2a6bf
chrissy <christinedonov@gmail.com>
2012-12-04 20:54:30 -0800

    Working on API.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,20 @@
+require 'json'
+
 class ApiController < ApplicationController
+	def new_event
+		puts "********************************************"
+		user 			= User.find_or_create_by_name(params["username"])
+		puts user
+		puts "*************************"
+		question_group	= params["event"]["problem_id"]
+		questions 		= params["event"]["answers"].keys
+		questions.each do |question|
+			question_id = (question_group + question).hash
+			Attempt.create(
+				:user_id => user.id,
+				:question_id => question_id,
+				:answer => params["event"]["answers"][question],
+				:is_correct => params["event"]["correct_map"][question]["correctness"] == "incorrect" ? false : true)
+		end		
+	end
 end
