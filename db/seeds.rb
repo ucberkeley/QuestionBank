907b7489893a6800f5933421f00172e16f41bf6a
Alexander Chen <alex7c@gmail.com>
2012-12-05 03:04:50 -0800

    Slightly more useful seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,102 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 require 'populator'
 require 'faker'
 
-# [Attempt, Question, Tag, User, Role].each(&:delete_all)
+# Create user groups
+5.times do |i|
+  UserGroup.create(:name => "User Group " + Faker::Name.name,)
+end
 
 # Create random users
 50.times do |i|
-  User.create(
-    :provider => 'facebook',
-    :uid => rand(100000).to_s,
+  u = User.create(
+    :provider => 'Twitter',
+    :uid => i+1,
     :name => Faker::Name.name,
     :email => Faker::Internet.email,
   )
+  (rand(3)+1).times do |j|
+    UserGroup.find(rand(5)+1).users << u
+  end
 end
-
-chrissy = User.find_or_create_by_name("Chrissy")
-joe = User.find_or_create_by_name("Joe")
-tett = User.find_or_create_by_name("Tett")
-alex = User.find_or_create_by_name("Alex")
-
-@users = User.all
-
-# Create user groups
-user_group_a = UserGroup.find_or_create_by_name(
-  :name => "UserGroupA"
-)
-user_group_b = UserGroup.find_or_create_by_name(
-  :name => "UserGroupB"
-)
-user_group_c = UserGroup.find_or_create_by_name(
-  :name => "UserGroupC"
-)
-
-# Put users in user groups
-user_group_a.users << chrissy
-user_group_b.users << tett
-user_group_c.users << alex
-user_group_c.users << joe
 
 # Create tags
 10.times do |i|
-  Tag.create(
-    :name => Populator.words(1),
-  )
+  Tag.create(:name => Populator.words(1))
 end
 
-@tags = Tag.all
+# Create questions
+5.times do |i|
+    QuestionGroup.create(:name => "Question Group " + Populator.words(1))
+end
 
 # Create questions
 25.times do |i|
-    user = @users.sample
-    user.questions.create(
-      :xml => Populator.words(1..5),
-    )
+    q = User.all.sample.questions.create(:xml => Populator.words(1..5))
+    (rand(3)+1).times do |j|
+      q.tags << Tag.all.sample
+    end
+    QuestionGroup.all.sample.questions << q
 end
-
-question_1 = Question.find_or_create_by_xml("Question 1")
-question_2 = Question.find_or_create_by_xml("Question 2")
-question_3 = Question.find_or_create_by_xml("Question 3")
-question_4 = Question.find_or_create_by_xml("Question 4")
-
-@questions = Question.all
-
-# Create question groups
-question_group_a = QuestionGroup.find_or_create_by_name_and_user_id(
-  "QuestionGroupA",
-  joe.id
-)
-question_group_b = QuestionGroup.find_or_create_by_name_and_user_id(
-  "QuestionGroupB",
-  alex.id
-)
-question_group_c = QuestionGroup.find_or_create_by_name_and_user_id(
-  "QuestionGroupC",
-  chrissy.id
-)
-
-@question_groups = QuestionGroup.all
-
-# Put questions in question groups
-question_group_a.questions << question_2
-question_group_a.questions << question_3
-question_group_b.questions << question_3
-question_group_b.questions << question_1
-question_group_c.questions << question_4
-question_group_c.questions << question_1
 
 # Create attempts
 50.times do |i| 
-  user = @users.sample
-  question = @questions.sample
+  user = User.all.sample
+  question = Question.all.sample
   Attempt.create(
     :user_id => user.id,
     :question_id => question.id,
@@ -104,16 +49,3 @@ question_group_c.questions << question_1
     :is_correct => [true, false].sample,
   )
 end    
-
-# Create questions_tags entries
-50.times do |i|
-  question = @questions.sample
-  tag = @tags.sample
-  question.tags << tag
-end
-
-
-# Roles
-
-
-# Abilities
